6dd2001d20ede5a272dee5d5daf11f9d499b7a21
Hari Kishan <hari.kishan@delhivery.com>
2013-11-25 21:12:59 +0530

    added into pypi

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Redis-DB',
+    version='0.1.0',
+    author='Hari Kishan',
+    author_email='hari.kishan81001@gmail.com',
+    packages=['redis_db'],
+    url='http://pypi.python.org/pypi/redis-db/',
+    license='LICENSE.txt',
+    description='Redis cache for managing Django ORM',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "Django >= 1.4",
+        "redis >= 2.7.4",
+    ],
+)
